9fec588c360a7785817158bc5242a3e67b7a527c
guozhaokui <guozhaokuiwang@gmail.com>
2019-12-27 20:29:46 +0800

    grid

---
+++ src/editor/gridSurface.ts
@@ -0,0 +1,79 @@
+import { Mesh } from "laya/d3/resource/models/Mesh";
+import { Vector3 } from "laya/d3/math/Vector3";
+import { Quaternion } from "laya/d3/math/Quaternion";
+import { PixelLineSprite3D } from "laya/d3/core/pixelLine/PixelLineSprite3D";
+import { Scene3D } from "laya/d3/core/scene/Scene3D";
+import { Color } from "laya/d3/math/Color";
+import { PixelLineMaterial } from "laya/d3/core/pixelLine/PixelLineMaterial";
+import { Material } from "laya/d3/core/material/Material";
+import { RenderState } from "laya/d3/core/material/RenderState";
+
+export class GridSurface {
+	private grid = new PixelLineSprite3D(32 * 1024);
+	private dyna = new PixelLineSprite3D(8 * 1024);
+	public showAxis = true;
+	private gridw = 10;
+	constructor(gridw: number, pos: Vector3 | null, quat: Quaternion | null) {
+		this.gridw = gridw;
+		let grid = this.grid;
+		if (pos) grid.transform.position = pos;
+		if (quat) grid.transform.rotation = quat;
+	}
+
+	private build() {
+		let xnum = 200;
+		let ynum = 200;
+		let gridw = this.gridw;
+		let minx = -xnum * gridw;
+		let miny = -ynum * gridw;
+		let maxx = xnum * gridw;
+		let maxy = ynum * gridw;
+
+		let xcolor = new Color(1, 0, 0, 1);
+		let ycolor = new Color(0, 1, 0, 1);
+		let zcolor = new Color(0, 0, 1, 1);
+		let color = new Color(0.5, 0.5, 0.5, 0.5);
+
+		let grid = this.grid;
+		for (let x = 0; x < xnum; x++) {
+			grid.addLine(new Vector3(gridw * x, 0, miny), new Vector3(gridw * x, 0, maxy), color, color);
+			grid.addLine(new Vector3(-gridw * x, 0, miny), new Vector3(-gridw * x, 0, maxy), color, color);
+		}
+
+		for (let y = 0; y < ynum; y++) {
+			grid.addLine(new Vector3(minx, 0, gridw * y), new Vector3(maxx, 0, gridw * y), color, color);
+			grid.addLine(new Vector3(minx, 0, -gridw * y), new Vector3(maxx, 0, -gridw * y), color, color);
+		}
+
+		if (this.showAxis) {
+			grid.addLine(new Vector3(minx, 0, 0), new Vector3(maxx, 0, 0), xcolor, xcolor);
+			grid.addLine(new Vector3(0, miny, 0), new Vector3(0, maxy, 0), ycolor, ycolor);
+			grid.addLine(new Vector3(0, 0, minx), new Vector3(0, 0, maxx), zcolor, zcolor);
+		}
+
+		var lineMaterial = new PixelLineMaterial();
+		lineMaterial.renderQueue = Material.RENDERQUEUE_TRANSPARENT;
+		lineMaterial.alphaTest = false;
+		lineMaterial.depthWrite = false;
+		lineMaterial.cull = RenderState.CULL_BACK;
+		lineMaterial.blend = RenderState.BLEND_ENABLE_ALL;
+		lineMaterial.blendSrc = RenderState.BLENDPARAM_SRC_ALPHA;
+		lineMaterial.blendDst = RenderState.BLENDPARAM_ONE_MINUS_SRC_ALPHA;
+		lineMaterial.depthTest = RenderState.DEPTHTEST_LESS;
+		grid.pixelLineRenderer.sharedMaterial = lineMaterial;
+
+	}
+
+	addToScene(sce: Scene3D) {
+		this.build();
+		sce.addChild(this.grid);
+		sce.addChild(this.dyna);
+	}
+
+	/**
+	 * 把碰撞点用横纵坐标显示出来
+	 */
+	showHitPoint() {
+
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import { Vector4 } from "laya/d3/math/Vector4";
 import { DirectionLight } from "laya/d3/core/light/DirectionLight";
 import { genVoxData, SphereData, Sine_Waves, Terrain, Perlin_Noise } from "./datagen/TestData";
 import { SurfaceNets } from "./SurfaceNets";
+import { GridSurface } from "./editor/gridSurface";
+import { Quaternion } from "laya/d3/math/Quaternion";
 
 //
 let scene: Scene3D;
@@ -21,11 +23,11 @@ scene = Laya.stage.addChild(new Scene3D()) as Scene3D;
 let mtl2 = new BlinnPhongMaterial();
 
 var camera = (<Camera>scene.addChild(new Camera(0, 1, 10000)));
-camera.transform.translate(new Vector3(0, 0, 6));
+//camera.transform.translate(new Vector3(0, 0, 6));
 //camera.transform.rotate(new Vector3(-15, 0, 0), true, false);
 let camctrl = camera.addComponent(MouseCtrl1) as MouseCtrl1;
 camera.clearColor=new Vector4(0.01,0,0,0);
-camctrl.initCamera(new Vector3(0, 0, 0), new Vector3(-15, 0, 0), 12);
+camctrl.initCamera(new Vector3(0, 0, 0), new Vector3(0, 0, 0), 132);
 //camera.addComponent(CameraMoveScript);
 //camera.clearColor = null;
 
@@ -48,7 +50,7 @@ function getdata(x:number,y:number,z:number):number{
 //scene.addChild(mesh);
 //mesh.transform.localPosition = new Vector3(-5,-5,-5)
 
-let sidelen=4;
+let sidelen=24;
 //let data = SphereData(-2,2,sidelen);
 let data = SphereData(-2,2,sidelen);
 
@@ -56,11 +58,17 @@ let isos = new SurfaceNets();
 let mesh1 = isos.tomesh(data,[sidelen,sidelen,sidelen]);
 //let mesh1 = isos.tomesh(new Float32Array([1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1]),[4,1,4]);
 
+let gridq =new Quaternion();
+Quaternion.createFromAxisAngle(new Vector3(1,0,0), Math.PI/4, gridq);
+let grid = new GridSurface(10,null,null);
+//grid.showAxis=false;
+grid.addToScene(scene);
+
 let meshes = creatQuadMesh(mesh1.vertices, mesh1.faces);
 meshes.forEach( mesh=>{
 	let cmesh = new MeshSprite3D(mesh);
 	scene.addChild(cmesh);
-	cmesh.transform.localPosition = new Vector3(-2,-2,-2)
+	cmesh.transform.localPosition = new Vector3(-10,0,0)
 });
 
 function printLayerx(n:int){
